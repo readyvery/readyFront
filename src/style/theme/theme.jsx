bf0055895423c30adbba66ff9e349b03d1efb796
imi21123 <imi21123@naver.com>
2023-10-27 20:27:06 +0900

    Config: 공통테마 설정

---
+++ src/style/theme/theme.jsx
@@ -0,0 +1,15 @@
+const theme = {
+    colors: {
+        MainColor: '#05422b',
+        HoverColor: '#3CB371',
+        accent: '#e0234e',
+    },
+    fontSize: {
+        small: '12px',
+        medium: '16px',
+        large: '24px',
+    },
+    // 여기에 더 많은 테마 변수를 추가할 수 있습니다.
+};
+
+export default theme;
