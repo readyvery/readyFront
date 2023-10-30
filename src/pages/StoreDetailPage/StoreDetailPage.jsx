5a9cb85ce4ce388e04ce52ccb88ae651e0ce7c46
imi21123 <imi21123@naver.com>
2023-10-30 21:57:40 +0900

    Config: text, image 구조 설정

---
+++ src/pages/StoreDetailPage/StoreDetailPage.jsx
@@ -0,0 +1,7 @@
+import { IMAGES } from "../../constants/images";
+import { TEXT } from "../../constants/text";
+import "./style.css";
+
+const StoreDetailPage = () => {};
+
+export default StoreDetailPage;
