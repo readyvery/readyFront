0af8416946dc2a075158e3938cb78d0b3df03762
imi21123 <imi21123@naver.com>
2023-11-04 04:17:53 +0900

    Feat: 홈화면 가게 버튼 추가, app.css 수정

--- src/pages/HomePage/Homepage.jsx
+++ src/pages/HomePage/Homepage.jsx
@@ -1,13 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Header from "../../components/views/Header/Header";
 import NavBar from "../../components/views/NavBar/NavBar";
+import ORDA from "../../assets/images/caffee_banner.svg";
 
 function Homepage() {
   return (
     <div>
       <Header />
+      <Link to="/cafe/:cafeId">
+        <img src={ORDA} alt="오르다" />
+      </Link>{" "}
       <NavBar />
-      <nav>Homepage!</nav>
     </div>
   );
 }
--- src/pages/StoreDetailPage/StoreDetailPage.jsx
+++ src/pages/StoreDetailPage/StoreDetailPage.jsx
@@ -1,9 +1,12 @@
 import caffeeBanner from "../../assets/images/caffee_banner.svg";
+import Header from "../../components/views/Header/Header";
+import NavBar from "../../components/views/NavBar/NavBar";
 import "./style.css";
 
 const StoreDetailPage = () => {
   return (
     <div className="store-detail-page">
+      <Header />
       <div className="store-detail-page__banner">
         <img
           className="store-detail-page__bannerImg"
@@ -47,6 +50,7 @@ const StoreDetailPage = () => {
           </text>
         </div>
       </div>
+      <NavBar />
     </div>
   );
 };
