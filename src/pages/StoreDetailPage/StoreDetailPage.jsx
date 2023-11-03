1c4e62f8a9a875a5c8eafa2950cd5407934fdbfe
imi21123 <imi21123@naver.com>
2023-11-03 22:53:55 +0900

    Feat: store detail page 구현 중

--- src/pages/StoreDetailPage/StoreDetailPage.jsx
+++ src/pages/StoreDetailPage/StoreDetailPage.jsx
@@ -1,7 +1,55 @@
-import { IMAGES } from "../../constants/images";
 import { TEXT } from "../../constants/text";
+import caffeeBanner from "../../assets/images/caffee_banner.svg";
 import "./style.css";
 
-const StoreDetailPage = () => {};
+const StoreDetailPage = () => {
+  return (
+    <div className="store-detail-page">
+      <div className="store-detail-page__banner">
+        <img
+          className="store-detail-page__bannerImg"
+          src={caffeeBanner}
+          alt="caffee banner"
+        />
+      </div>
+      <div className="store-detail-page__caffeeInfo">
+        <div className="store-detail-page__caffeeInfo__title">{"카페명"}</div>
+
+        <div className="store-detail-page__caffeeInfo__list">
+          <text className="store-detail-page__caffeeInfo__list__title">
+            {"연락처"}
+          </text>
+          <text className="store-detail-page__caffeeInfo__contact">
+            {"010-1234-5678"}
+          </text>
+        </div>
+
+        <div
+          className="store-detail-page__caffeeInfo__list"
+          style={{ marginTop: "0.5rem", marginBottom: "0.5rem" }}
+        >
+          <text className="store-detail-page__caffeeInfo__list__title">
+            {"주소"}
+          </text>
+          <text className="store-detail-page__caffeeInfo__address">
+            {"서울시 강남구 테헤란로 427"}
+          </text>
+        </div>
+
+        <div
+          className="store-detail-page__caffeeInfo__list"
+          style={{ marginBottom: "1.37rem" }}
+        >
+          <text className="store-detail-page__caffeeInfo__list__title">
+            {"영업 시간"}
+          </text>
+          <text className="store-detail-page__caffeeInfo__time">
+            {"주중 9:30 - 22:30, 토요일 10:00 - 22:00, 일요일 휴무"}
+          </text>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default StoreDetailPage;
