77a31ef57eae188a00ebe84a93553e48272e85d0
hhhhyelim <yanghl0527@naver.com>
2023-11-13 22:21:14 +0900

    Feat: 로그인 구현
    - 카카오 리다이렉트

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import MyPage from "../src/pages/MyPage/MyPage";
 import StoreDetailPage from "../src/pages/StoreDetailPage/StoreDetailPage";
 import TestPage from "../src/pages/TestPage/TestPage";
 import "./App.css";
+import KakaoLoginPage from "./pages/LoginPage/Login";
+import CouponPage from "./pages/MyPage/CouponPage";
+import EventingPage from "./pages/MyPage/EventingPage";
+import MyprofilePage from "./pages/MyPage/MyprofilePage";
+import PolicyPage from "./pages/MyPage/PolicyPage";
 import PackagingStatusPage from "./pages/PackagingStatusPage/PackagingStatusPage";
 
 function App() {
@@ -23,13 +28,21 @@ function App() {
         {/* 테스트 페이지 - 마이페이지*/}
         <Route path="/mypage" element={<MyPage />} />
         {/* 테스트 페이지 - 로그인*/}
-        <Route path="/login" element={<TestPage />} />
+        <Route path="/kakaologin" element={<KakaoLoginPage />} />
         {/* 테스트 페이지 - 이벤트*/}
         <Route path="/event" element={<TestPage />} />
         {/* 카페 상세 페이지*/}
         <Route path="/storeDetail" element={<StoreDetailPage />} />
         {/* 포장 여부 선택 페이지 */}
         <Route path="/packagingStatus" element={<PackagingStatusPage />} />
+        {/* 마이페이지-쿠폰 확인 페이지 */}
+        <Route path="/coupon" element={<CouponPage />} />
+        {/* 마이페이지-마이프로필 확인 페이지 */}
+        <Route path="/myprofile" element={<MyprofilePage />} />
+        {/* 마이페이지-이벤트 페이지 */}
+        <Route path="/eventing" element={<EventingPage />} />
+        {/* 마이페이지-약관정책 페이지 */}
+        <Route path="/policy" element={<PolicyPage />} />
       </Routes>
     </div>
   );
---
+++ src/pages/MyPage/CouponPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../../components/views/Header/Header";
+import "./CouponPage.css";
+
+function CouponPage() {
+  return (
+    <div className="coupon-div">
+      <Header />
+      <div>쿠폰은 백에서 어떻게 가져올까유....</div>
+    </div>
+  );
+}
+
+export default CouponPage;
