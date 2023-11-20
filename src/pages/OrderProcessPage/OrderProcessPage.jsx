1287fd646fc3355905e8998b963e20304b01687a
happppi <102276917+hhbb0081@users.noreply.github.com>
2023-11-20 21:26:37 +0900

    Docs: 주문과정 api 연동

--- src/pages/OrderProcessPage/OrderProcessPage.jsx
+++ src/pages/OrderProcessPage/OrderProcessPage.jsx
@@ -8,27 +8,28 @@ import Header from "../../components/views/Header/Header";
 import "./style.css";
 
 const OrderProcessPage = () => {
-  const baseUrl = 'https://test.readyvery.com';
   const location = useLocation();
   const params = new URLSearchParams(location.search);
   const storeId = params.get("storeId");
   const inout = params.get("inout");
   // const foodie_id = params.get("foodie_id");
-  
-  const fetchData = async () => {
-    try{
-      const response = axios.get(
-        `${baseUrl}/api/v1/order/${storeId}?foody_id=1&inout=${inout}`,
-        {withCredentials: true}
-      );
-      console.log(response);
-    } catch(error) {
-      console.log(error);
-    }
-  }
+  const [foodOptionInfo, setFoodOptionInfo] = useState({});
   
   useEffect(() => {
+    const fetchData = async () => {
+      try{
+        const response = await axios.get(
+          `${process.env.REACT_APP_API_ROOT}/api/v1/order/${storeId}?foody_id=13&inout=${inout}`,
+          {withCredentials: true}
+        );
+        console.log(response.data);
+        setFoodOptionInfo(response.data);
+      } catch(error) {
+        console.log(error);
+      }
+    };
     fetchData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   // const foodOptionInfo = {
@@ -78,54 +79,60 @@ const OrderProcessPage = () => {
   //   ],
   // };
 
-  const foodOptionInfo = {
-    name:"에스프레소",
-    imgUrl:null,
-    price:2500,
-    category:
-    [
-      {
-        name:"HOT/ICE",
-        essential:true,
-        options:[
-          {"idx":1,"name":"HOT","price":0},
-          {"idx":2,"name":"ICE","price":500}
-        ]
-      },
-      {
-        name:"샷추가",
-        essential:true,
-        options:[
-          {"idx":3,"name":"연하게","price":0},
-          {"idx":4,"name":"1샷 추가","price":500}
-        ]
-      },
-      {
-        name:"휘핑",
-        essential:false,
-        options:[
-          {"idx":6,"name":"휘핑추가","price":1000}
-        ]
-      }
-    ]
-  };
+  // const foodOptionInfo = {
+  //   name:"에스프레소",
+  //   imgUrl:null,
+  //   price:2500,
+  //   category:
+  //   [
+  //     {
+  //       name:"HOT/ICE",
+  //       essential:true,
+  //       options:[
+  //         {"idx":1,"name":"HOT","price":0},
+  //         {"idx":2,"name":"ICE","price":500}
+  //       ]
+  //     },
+  //     {
+  //       name:"샷추가",
+  //       essential:true,
+  //       options:[
+  //         {"idx":3,"name":"연하게","price":0},
+  //         {"idx":4,"name":"1샷 추가","price":500}
+  //       ]
+  //     },
+  //     {
+  //       name:"휘핑",
+  //       essential:false,
+  //       options:[
+  //         {"idx":6,"name":"휘핑추가","price":1000}
+  //       ]
+  //     }
+  //   ]
+  // };
 
+// foodOptionInfo?.category?.map((_, i) => i === 0)
   const [activeToggles, setActiveToggles] = useState(
-    foodOptionInfo.category.map((_, i) => i === 0)
+    foodOptionInfo?.category?.map(() => false)
   );
   const [selectedRadioTexts, setSelectedRadioTexts] = useState(
-    foodOptionInfo.category.map((e) => `${e.options[0].name}`)
+    foodOptionInfo && foodOptionInfo.category?.map((e) => `${e.options[0]?.name}`)
   );
-  const [totalAmount, setTotalAmount] = useState(
-    foodOptionInfo.price + parseInt(foodOptionInfo.category.map((e) => parseInt(e.options[0].price)).reduce((prev, curr) => prev + curr, 0))
-  );
+  // const [totalAmount, setTotalAmount] = useState(
+  //     foodOptionInfo?.price + parseInt(foodOptionInfo?.category?.map((e) => parseInt(e?.options[0]?.price)).reduce((prev, curr) => prev + curr, 0))
+  // );
+  const [totalAmount, setTotalAmount] = useState(foodOptionInfo && foodOptionInfo.price && foodOptionInfo?.price);
+  useEffect(() => {
+    setTotalAmount(foodOptionInfo?.price + parseInt(foodOptionInfo?.category?.map((e) => parseInt(e?.options[0]?.price)).reduce((prev, curr) => prev + curr, 0)));
+  }, [foodOptionInfo]);
   const [prevRadioPrice, setPrevRadioPrice] = useState(
-    foodOptionInfo.category.map(() => 0)
+    foodOptionInfo?.category?.map(() => 0)
   );
 
   const handleToggle = (index) => {
     setActiveToggles((prevToggles) => {
-      const toggles = [...prevToggles];
+      console.log(prevToggles);
+      const toggles = prevToggles ? [...prevToggles] : [];
       toggles[index] = !toggles[index];
       return toggles;
     });
@@ -133,12 +140,12 @@ const OrderProcessPage = () => {
 
   const handleRadioChange = (index, text, price) => {
     setSelectedRadioTexts((prevTexts) => {
-      const texts = [...prevTexts];
+      const texts = prevTexts ? [...prevTexts] : [];
       texts[index] = text;
       return texts;
     });
 
-    setTotalAmount((prevAmount) => prevAmount - prevRadioPrice[index] + price);
+    setTotalAmount((prevAmount) => prevAmount - prevRadioPrice?.length && prevRadioPrice[index] + price);
 
     setPrevRadioPrice((prevPrices) => {
       const prices = [...prevPrices];
@@ -158,15 +165,15 @@ const OrderProcessPage = () => {
       />
 
       <div className="order-process-page__menu__img">
-        <img src={foodOptionInfo.imgUrl || noImageMenu} alt="menuImg" />
+        <img src={foodOptionInfo?.imgUrl || noImageMenu} alt="menuImg" />
       </div>
 
       <div className="order-process-page__menu__name">
-        {foodOptionInfo.name}
+        {foodOptionInfo?.name}
       </div>
 
       <div className="order-process-page__toggle">
-        {foodOptionInfo.category.map((category, index) => (
+        {foodOptionInfo?.category?.map((category, index) => (
           <div className="order-process-page__toggle__container" key={index}>
             <div
               className="order-process-page__toggle__header"
@@ -176,29 +183,29 @@ const OrderProcessPage = () => {
                 {category.name}
               </span>
               <span className="order-process-page__toggle__btn">
-                {selectedRadioTexts[index] && (
+                {selectedRadioTexts?.length && selectedRadioTexts[index] && (
                   <span className="order-process-page__selected-radio">
                     {selectedRadioTexts[index]}
                   </span>
                 )}
                 <img
                   className="order-process-page__toggle__header__img"
-                  src={activeToggles[index] ? toggleUp : toggleDown}
-                  alt={activeToggles[index] ? "Toggle Up" : "Toggle Down"}
+                  src={(activeToggles?.length && activeToggles[index]) ? toggleUp : toggleDown}
+                  alt={(activeToggles?.length && activeToggles[index]) ? "Toggle Up" : "Toggle Down"}
                 />
               </span>
             </div>
 
-            {activeToggles[index] && (
+            {activeToggles?.length && activeToggles[index] && (
               <div 
-                className={`order-process-page__toggle__body ${activeToggles[index] && "open"}`}
+                className={`order-process-page__toggle__body ${activeToggles?.length && activeToggles[index] && "open"}`}
                 style={{
                   maxHeight: "100%",
                   paddingTop: "1.56rem",
                   paddingBottom: "1.91rem",
                 }}
               >
-                {category.options.map((option, optionIndex) => (
+                {category?.options?.map((option, optionIndex) => (
                   <div
                     className="order-process-page__toggle__body__radio"
                     key={optionIndex}
@@ -211,7 +218,7 @@ const OrderProcessPage = () => {
                       type="radio"
                       name={`radio_${index}`}
                       value={optionIndex}
-                      checked={selectedRadioTexts[index] === option.name}
+                      checked={selectedRadioTexts?.length && selectedRadioTexts[index] === option.name}
                       onChange={() =>
                         handleRadioChange(index, option.name, option.price)
                       }
