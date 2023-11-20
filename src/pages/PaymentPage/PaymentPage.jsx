182646c2c0cb8fd235a07a0f32374cc367da6b42
imi21123 <imi21123@naver.com>
2023-11-21 03:54:13 +0900

    Fix: 사용하지 않는 코드 제거

--- src/pages/PaymentPage/PaymentPage.jsx
+++ src/pages/PaymentPage/PaymentPage.jsx
@@ -96,7 +96,6 @@ const PaymentPage = () => {
       })
       .then((res) => {
         console.log(res.data);
-        setTosspayment(res.data);
 
         const paymentWidget = paymentWidgetRef.current;
 
