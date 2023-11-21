ae97280771b1d8aa2b2a79f99056257a9ccee389
imi21123 <imi21123@naver.com>
2023-11-21 23:09:58 +0900

    Feat: 리다이렉트 페이지 제작 중

--- src/App.js
+++ src/App.js
@@ -20,6 +20,7 @@ import PackagingStatusPage from "./pages/PackagingStatusPage/PackagingStatusPage
 import PaymentPage from "./pages/PaymentPage/PaymentPage";
 import ReadyPage from "./pages/ReadyPage/ReadyPage";
 import PaymentSuccessPage from "./pages/PaymentPage/Redirect/PaymentSuccessPage";
+import PaymentFailPage from "./pages/PaymentPage/Redirect/PaymentFailPage";
 
 function App() {
   return (
@@ -63,9 +64,10 @@ function App() {
         <Route path="/cart" element={<CartPage />} />
         {/* 결제 페이지 */}
         <Route path="/payment" element={<PaymentPage />} />
-        {/* 결제 성공 페이지 */}
-        <Route path="/paymentSuccess" element={<PaymentSuccessPage />} />
-        {/* 결제 실패 페이지 */}
+        {/* 결제 성공 리다이렉트 페이지 */}
+        <Route path="/payment/success" element={<PaymentSuccessPage />} />
+        {/* 결제 실패 리다이렉트 페이지 */}
+        <Route path="/payment/fail" element={<PaymentFailPage />} />
       </Routes>
     </div>
   );
---
+++ src/pages/PaymentPage/Redirect/PaymentFailPage.jsx
@@ -0,0 +1,8 @@
+const PaymentFailPage = () => {
+  return (
+    <div>
+      <h1>Payment Fail</h1>
+    </div>
+  );
+};
+export default PaymentFailPage;
