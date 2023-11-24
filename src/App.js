2e71e0daa982cadab9de8ba7ac05f5a15aa4c71f
hhhhyelim <yanghl0527@naver.com>
2023-11-24 15:49:20 +0900

    Feat: 홈화면 로그인 구현

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import { Navigate, Route, Routes } from "react-router-dom";
+import axios from "axios";
+import React, { Suspense } from "react";
+import { useCookies } from "react-cookie";
+import { Route, Routes, useNavigate } from "react-router-dom";
+import { RecoilRoot } from "recoil";
 import HomePage from "../src/pages/HomePage/Homepage";
 import MyPage from "../src/pages/MyPage/MyPage";
 import StoreDetailPage from "../src/pages/StoreDetailPage/StoreDetailPage";
 import "./App.css";
 import Auth from "./hoc/auth";
+import useInterval from "./hooks/useInterval";
 import CafeSearchPage from "./pages/CafeSearch/CafeSearch";
 import CartPage from "./pages/CartPage/CartPage";
 import KakaoLoginPage from "./pages/LoginPage/Login";
@@ -26,6 +31,9 @@ import PaymentSuccessPage from "./pages/PaymentPage/Redirect/PaymentSuccessPage"
 import ReadyPage from "./pages/ReadyPage/ReadyPage";
 
 function App() {
+  const [cookies, , removeCookies] = useCookies();
+  const navigate = useNavigate();
+  const apiUrl = process.env.REACT_APP_API_ROOT;
   // const navigate = useNavigate();
   // // const [isLoggedIn, setisLoggedIn] = useRecoilState(isAuthenticatedState); // 인증 상태 (로그인이 되어있으면 true, 아니면 false)
   // // 토큰 받아서 시간관리
@@ -51,95 +59,96 @@ function App() {
   const NewPaymentFailPage = Auth(PaymentFailPage, true);
   const NewPackagingStatusPage = Auth(PackagingStatusPage, true);
 
-  // const minute = 1000 * 60 * 60 * 24; // 24시간
-  // // 주기적으로 실행되는 함수
-  // useInterval(() => {
-  //   // 리프레시 토큰이 존재하고, 비어 있지 않은 경우
-  //   if (
-  //     cookies.refreshToken !== "undefined" &&
-  //     cookies.refreshToken !== undefined &&
-  //     cookies.refreshToken
-  //   ) {
-  //     // http 요청에 사용될 헤더 설정과 함께 서버에 토큰 갱신 요청
-  //     let config = {
-  //       headers: {
-  //         Authorization: `${cookies.refreshToken}`,
-  //       },
-  //       withCredentials: true,
-  //     };
-  //     axios
-  //       .get(`${process.env.REACT_APP_API_ROOT}/api/v1/refresh/token`, config)
-  //       .then((response) => {
-  //         // 현재 쿠키 삭제
-  //         removeCookies();
-
-  //         // 서버에서 받은 새로운 리프레시 토큰을 쿠키에 저장
-  //         setCookie("refreshToken", response.data.refreshToken);
-
-  //         // 홈 화면으로 이동
-  //         navigate("/");
-  //       })
-
-  //       .catch((error) => {
-  //         navigate("/kakaologin");
-  //       });
-  //   } else {
-  //     // 리프레시 토큰이 존재하지 않거나 비어 있으면 쿠키 제거
-  //     removeCookies();
-  //   }
-  // }, minute - 60000); // 24시간 주기에서 1분을 뺀 주기로 주기적 실행
+  const minute = 1000 * 60 * 60 * 24; // 24시간
+  // 주기적으로 실행되는 함수
+  useInterval(() => {
+    // 리프레시 토큰이 존재하고, 비어 있지 않은 경우
+    if (
+      cookies.refreshToken !== "undefined" &&
+      cookies.refreshToken !== undefined &&
+      cookies.refreshToken
+    ) {
+      // http 요청에 사용될 헤더 설정과 함께 서버에 토큰 갱신 요청
+      let config = {
+        withCredentials: true,
+      };
+      axios
+        .get(`${apiUrl}/api/v1/refresh/token`, config)
+        .then((response) => {
+          console.log(response);
+          // 현재 쿠키 삭제
+          if (!response.data) {
+            removeCookies();
+            navigate("/kakaologin");
+          }
+        })
+        .catch((error) => {
+          navigate("/kakaologin");
+        });
+    }
+  }, minute - 60000); // 24시간 주기에서 1분을 뺀 주기로 주기적 실행
 
   return (
     <div className="App">
-      <Routes>
-        {/* 로그인 하지 않아도 볼 수 있는 페이지 */}
-        {/* 메인페이지 */}
-        <Route path="/" element={<HomePage />} />
-        {/* 없는 경로로 갈 경우 메인페이지로 강제 이동 */}
-        <Route path="/*" element={<Navigate to="/"></Navigate>}></Route>
-        {/* 카페검색*/}
-        <Route path="/search" element={<CafeSearchPage />} />
-        {/* 로그인*/}
-        <Route path="/kakaologin" element={<NewLoginPage />} />
-        {/* 마이페이지-약관정책 페이지 */}
-        <Route path="/policy" element={<PolicyPage />} />
-        <Route path="/termsofuse" element={<TermsOfUse />} />
-        <Route path="/privacypolicy" element={<PrivacyPolicy />} />
-        <Route path="/thirdparty" element={<ThirdpartyPage />} />
-        <Route path="/position" element={<PositionpolicyPage />} />
-        {/* 마이페이지-이벤트 페이지 */}
-        <Route path="/eventing" element={<EventingPage />} />
-        {/* 카페 상세 페이지*/}
-        <Route path="/store" element={<StoreDetailPage />} />
-        {/* 마이페이지*/}
-        <Route path="/mypage" element={<MyPage />} />
+      <RecoilRoot>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            {/* 로그인 하지 않아도 볼 수 있는 페이지 */}
+            {/* 메인페이지 */}
+            <Route path="/" element={<HomePage />} />
+            {/* 없는 경로로 갈 경우 메인페이지로 강제 이동 */}
+            {/* <Route path="/*" element={<Navigate to="/"></Navigate>}></Route> */}
+            {/* 카페검색*/}
+            <Route path="/search" element={<CafeSearchPage />} />
+            {/* 로그인*/}
+            <Route path="/kakaologin" element={<NewLoginPage />} />
+            {/* 마이페이지-약관정책 페이지 */}
+            <Route path="/policy" element={<PolicyPage />} />
+            <Route path="/termsofuse" element={<TermsOfUse />} />
+            <Route path="/privacypolicy" element={<PrivacyPolicy />} />
+            <Route path="/thirdparty" element={<ThirdpartyPage />} />
+            <Route path="/position" element={<PositionpolicyPage />} />
+            {/* 마이페이지-이벤트 페이지 */}
+            <Route path="/eventing" element={<EventingPage />} />
+            {/* 카페 상세 페이지*/}
+            <Route path="/store" element={<StoreDetailPage />} />
+            {/* 마이페이지*/}
+            <Route path="/mypage" element={<MyPage />} />
 
-        {/* 로그인 해야지 볼 수 있는 페이지 */}
-        {/* 주문내역 페이지*/}
-        <Route path="/storage" element={<NewOrderStorage />} />
-        {/* 주문상태 페이지 - 추후 병합 예정*/}
-        <Route path="/status" element={<NewOrderStatus />} />
-        {/* 주문상세 페이지 - 추후 병합 예정*/}
-        <Route path="/orderDetail" element={<NewOrderDetail />} />
-        {/* 바로주문*/}
-        <Route path="/ready" element={<NewReadyPage />} />
-        {/* 마이페이지-마이프로필 확인 페이지 */}
-        <Route path="/myprofile" element={<NewMyprofilePage />} />
-        {/* 포장 여부 선택 페이지 */}
-        <Route path="/packagingStatus" element={<NewPackagingStatusPage />} />
-        {/* 주문 과정 페이지 */}
-        <Route path="/order" element={<NewOrderProcessPage />} />
-        {/* 마이페이지-쿠폰 확인 페이지 */}
-        <Route path="/coupon" element={<NewCouponPage />} />
-        {/* 장바구니 페이지 */}
-        <Route path="/cart" element={<NewCartPage />} />
-        {/* 결제 페이지 */}
-        <Route path="/payment" element={<NewPaymentPage />} />
-        {/* 결제 성공 리다이렉트 페이지 */}
-        <Route path="/payment/success" element={<NewPaymentSuccessPage />} />
-        {/* 결제 실패 리다이렉트 페이지 */}
-        <Route path="/payment/fail" element={<NewPaymentFailPage />} />
-      </Routes>
+            {/* 로그인 해야지 볼 수 있는 페이지 */}
+            {/* 주문내역 페이지*/}
+            <Route path="/storage" element={<NewOrderStorage />} />
+            {/* 주문상태 페이지 - 추후 병합 예정*/}
+            <Route path="/status" element={<NewOrderStatus />} />
+            {/* 주문상세 페이지 - 추후 병합 예정*/}
+            <Route path="/orderDetail" element={<NewOrderDetail />} />
+            {/* 바로주문*/}
+            <Route path="/ready" element={<NewReadyPage />} />
+            {/* 마이페이지-마이프로필 확인 페이지 */}
+            <Route path="/myprofile" element={<NewMyprofilePage />} />
+            {/* 포장 여부 선택 페이지 */}
+            <Route
+              path="/packagingStatus"
+              element={<NewPackagingStatusPage />}
+            />
+            {/* 주문 과정 페이지 */}
+            <Route path="/order" element={<NewOrderProcessPage />} />
+            {/* 마이페이지-쿠폰 확인 페이지 */}
+            <Route path="/coupon" element={<NewCouponPage />} />
+            {/* 장바구니 페이지 */}
+            <Route path="/cart" element={<NewCartPage />} />
+            {/* 결제 페이지 */}
+            <Route path="/payment" element={<NewPaymentPage />} />
+            {/* 결제 성공 리다이렉트 페이지 */}
+            <Route
+              path="/payment/success"
+              element={<NewPaymentSuccessPage />}
+            />
+            {/* 결제 실패 리다이렉트 페이지 */}
+            <Route path="/payment/fail" element={<NewPaymentFailPage />} />
+          </Routes>
+        </Suspense>
+      </RecoilRoot>
     </div>
   );
 }
