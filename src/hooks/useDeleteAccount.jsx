de5dadbfef1e9cb3831d2370721355b7d1a9290a
imi21123 <imi21123@naver.com>
2024-01-04 20:05:57 +0900

    Feat: MyPage/Myprofilepage axios 분리

---
+++ src/hooks/useDeleteAccount.jsx
@@ -0,0 +1,32 @@
+// hooks/useDeleteAccount.js
+import axios from "axios";
+import { message } from "antd";
+import { useNavigate } from "react-router-dom";
+
+const apiRoot = process.env.REACT_APP_API_ROOT;
+
+const useDeleteAccount = (removeCookie, setIsAuth) => {
+  const navigate = useNavigate();
+
+  const deleteAccount = async () => {
+    try {
+      const response = await axios.get(`${apiRoot}/api/v1/user/remove`, {
+        withCredentials: true,
+      });
+      console.log(response);
+      removeCookie("accessToken", { domain: process.env.REACT_APP_DOMAIN });
+      removeCookie("JSESSIONID", { domain: process.env.REACT_APP_DOMAIN });
+      setIsAuth(false);
+      message.success("회원탈퇴에 성공하셨습니다.");
+      navigate("/");
+    } catch (error) {
+      console.error("Error during account deletion:", error);
+      message.error("회원탈퇴 실패. 관리자에게 문의하세요.");
+      navigate("/");
+    }
+  };
+
+  return deleteAccount;
+};
+
+export default useDeleteAccount;
---
+++ src/hooks/useFetchUserInfo.jsx
@@ -0,0 +1,29 @@
+// hooks/useFetchUserInfo.js
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const apiRoot = process.env.REACT_APP_API_ROOT;
+
+const useFetchUserInfo = () => {
+  const [userInfo, setUserInfo] = useState({ name: "", phone: "", email: "" });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`${apiRoot}/api/v1/user/info`, {
+          withCredentials: true,
+        });
+        const { name, phone, email } = response.data;
+        setUserInfo({ name, phone, email });
+      } catch (error) {
+        console.error("Error fetching user info:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return userInfo;
+};
+
+export default useFetchUserInfo;
