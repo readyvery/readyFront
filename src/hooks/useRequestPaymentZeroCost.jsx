f2f8afcff012b35dd8902daca3ff10796d763171
KKangHHee <137751841+KKangHHee@users.noreply.github.com>
2024-02-17 12:59:08 +0900

    Feat: 0원 결제 api연결 누락 추가

--- src/hooks/useRequestPaymentZeroCost.jsx
+++ src/hooks/useRequestPaymentZeroCost.jsx
@@ -10,8 +10,6 @@ const useRequestZoreCostPayment = () => {
       `paymentKey=${zoreCostPaymentKey}&` +
       `orderId=${orderId}&` +
       `amount=${amount}`;
-
-    // http://localhost:3000/payment/success?paymentType=NORMAL&orderId=83a6173d-36dd-4a09-a6f5-a31e11cdfc9c&paymentKey=y0E4Dv5qpMGjLJoQ1aVZkPyEMdq9Arw6KYe2RNgOWznZb7Bm&amount=7500
     try {
       window.location.href = apiUrl;
       return;
