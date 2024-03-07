fee30bd3e06b6e8d2cd8c355a519c99d3109d212
imi21123 <imi21123@naver.com>
2024-03-07 20:37:45 +0900

    Style: Login UI 수정

--- src/pages/LoginPage/Login.jsx
+++ src/pages/LoginPage/Login.jsx
@@ -18,7 +18,7 @@ function Login() {
 
       <img src={IMAGES.berryLogo} alt="ReadyVery" className="login_logo" />
 
-      <div className="login_slogan">
+      {/* <div className="login_slogan">
         <span className="login_slogan_text">준비는 빠르게 혜택은 다르게</span>
       </div>
 
@@ -28,7 +28,20 @@ function Login() {
         className="login_kakao"
         onClick={handleKakaoLogin}
       />
-      <img src={IMAGES.appleLogin} alt="appleLogin" className="login_apple" />
+      <img src={IMAGES.appleLogin} alt="appleLogin" className="login_apple" /> */}
+
+      <div className="login_content">
+        준비는 빠르게 혜택은 다르게
+        <div className="login_btn">
+          <img src={IMAGES.googleLogin} alt="googleLogin" />
+          <img src={IMAGES.appleLogin} alt="appleLogin" />
+          <img
+            src={IMAGES.kakaoLogin}
+            alt="kakaoLogin"
+            onClick={handleKakaoLogin}
+          />
+        </div>
+      </div>
     </div>
   );
 }
