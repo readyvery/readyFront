36926a8406c1a9666759ed42e228e29d4c6d4106
happppi <102276917+hhbb0081@users.noreply.github.com>
2024-03-08 13:03:24 +0900

    Feat: google, apple 로그인 추가

--- src/pages/LoginPage/Login.jsx
+++ src/pages/LoginPage/Login.jsx
@@ -1,11 +1,19 @@
-import "./Login.css";
-import { IMAGES } from "../../constants/images";
 import React from "react";
+import { IMAGES } from "../../constants/images";
+import "./Login.css";
 
 function Login() {
   const handleKakaoLogin = () => {
     window.location.href = `${process.env.REACT_APP_KAKAO_LOGIN}`;
   };
+
+  const handleAppleLogin = () => {
+    window.location.href = `${process.env.REACT_APP_APPLE_LOGIN}`;
+  }
+
+  const handleGoogleLogin = () => {
+    window.location.href = `${process.env.REACT_APP_GOOGLE_LOGIN}`
+  }
 
   return (
     <div className="login">
@@ -22,13 +30,27 @@ function Login() {
         <span className="login_slogan_text">준비는 빠르게 혜택은 다르게</span>
       </div>
 
+      {/* 카카오 로그인 */}
       <img
         src={IMAGES.kakaoLogin}
         alt="kakaoLogin"
         className="login_kakao"
         onClick={handleKakaoLogin}
       />
-      <img src={IMAGES.appleLogin} alt="appleLogin" className="login_apple" />
+      {/* 애플 로그인 */}
+      <img 
+        src={IMAGES.appleLogin} 
+        alt="appleLogin" 
+        className="login_apple" 
+        onClick={handleAppleLogin}
+      />
+      {/* 구글 로그인 */}
+      <img 
+        src={IMAGES.appleLogin} 
+        alt="googleLogin" 
+        className="login_apple" 
+        onClick={handleGoogleLogin}
+      />
     </div>
   );
 }
