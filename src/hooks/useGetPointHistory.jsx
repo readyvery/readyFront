4a0b3f6c11019463b9a8bdf251609ca7fe20dfc3
KKangHHee <skh8609@gmail.com>
2024-03-26 21:55:00 +0900

    Feat: 로딩중 적용

--- src/hooks/useGetPointHistory.jsx
+++ src/hooks/useGetPointHistory.jsx
@@ -6,24 +6,27 @@ const apiUrl = `/point/history`;
 // 포인트 내역 조회
 const useGetPointHistory = () => {
   const token = localStorage.getItem("accessToken");
+  const [isLoadingPointHistory, setIsLoadingPointHistory] = useState(true);
   const [pointHistory, setPointHistory] = useState([]);
-    useEffect(() => {
-        const fetchPointHistory = async () => {
-            try {
-                const res = await commonApis.get(apiUrl, {
-                  headers: {
-                      Authorization: `Bearer ${token}`
-                  }
-              });
-                setPointHistory(res.data.history);
-            } catch (error) {
-                console.error("Error fetching new order history:", error);
-            }
-        };
-        fetchPointHistory();
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
-  return pointHistory;
+  useEffect(() => {
+    const fetchPointHistory = async () => {
+      try {
+        const res = await commonApis.get(apiUrl, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        setPointHistory(res.data.history);
+      } catch (error) {
+        console.error("Error fetching new order history:", error);
+      } finally {
+        setIsLoadingPointHistory(false);
+      }
+    };
+    fetchPointHistory();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  return { pointHistory, isLoadingPointHistory };
 };
 
 export default useGetPointHistory;
