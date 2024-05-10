70b2343ee80558f7bcde62f25899ab6001b3a925
KKangHHee <skh8609@gmail.com>
2024-05-10 14:36:31 +0900

    Feat: 축제용 배너 추가

--- src/components/views/Home/Banner/Banner.jsx
+++ src/components/views/Home/Banner/Banner.jsx
@@ -1,46 +1,55 @@
-import React, { useState } from "react";
-import Slider from "react-slick";
+import React from "react";
+import { useNavigate } from "react-router-dom";
 import "slick-carousel/slick/slick-theme.css";
 import "slick-carousel/slick/slick.css";
+import { IMAGES } from "../../../../constants/images";
 import "./Banner.css";
 
 const Banner = () => {
-  const eventBanners = [
-    { bannerImg: "https://readyvery.com/statics/images/event/ic_home_event1.png" },
-    { bannerImg: "https://readyvery.com/statics/images/event/ic_home_event2.png" },
-    { bannerImg: "https://readyvery.com/statics/images/event/ic_home_event3.png" },
-  ];
-  
-  const [currentIndex, setCurrentIndex] = useState(0);
+  const navigate = useNavigate();
+  // const eventBanners = [
+  //   { bannerImg: "https://readyvery.com/statics/images/event/ic_home_event1.png" },
+  //   { bannerImg: "https://readyvery.com/statics/images/event/ic_home_event2.png" },
+  //   { bannerImg: "https://readyvery.com/statics/images/event/ic_home_event3.png" },
+  // ];
 
-  const settings = {
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    centerMode: true,
-    autoplay: true,
-    variableWidth: true,
-    autoplaySpeed: 1800, //1.8s
-    afterChange: (currentSlide) => setCurrentIndex(currentSlide), //이게 인덱스값 업데이트용
-  };
+  // const [currentIndex, setCurrentIndex] = useState(0);
+
+  // const settings = {
+  //   slidesToShow: 1,
+  //   slidesToScroll: 1,
+  //   centerMode: true,
+  //   autoplay: true,
+  //   variableWidth: true,
+  //   autoplaySpeed: 1800, //1.8s
+  //   afterChange: (currentSlide) => setCurrentIndex(currentSlide), //이게 인덱스값 업데이트용
+  // };
 
   return (
     <div className="home_event">
       <div className="home_event_slider_benner_wrapper">
-        <Slider {...settings} >
+        {/* <Slider {...settings} >
           {eventBanners.map((item) => {
             return (
               <div
                 className="home_event_slider_benner"
                 key={item}
+                onClick={() => navigate(`/search`)}
               >
                 <img src={item.bannerImg} alt={`Banner ${item}`} />
               </div>
             );
           })}
-        </Slider>
-        <div className="home_event_slide_index">
+        </Slider> */}
+        {/* <div className="home_event_slide_index">
           <span className="home_event_slide_index_now">{currentIndex + 1}</span>
           /{eventBanners.length}
+        </div> */}
+        <div
+          className="home_event_slider_benner"
+          onClick={() => navigate(`/booth`)}
+        >
+          <img src={IMAGES.banner_img} alt={`Banner`} />
         </div>
       </div>
     </div>
